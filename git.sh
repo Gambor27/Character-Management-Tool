2c28caf824a3bac1018d797ac56e3d0d17bbed6a
Gambor27 <gambor27@gmail.com>
2024-03-17 15:57:56 -0700

    New Project

---
+++ git.sh
@@ -0,0 +1,7 @@
+echo "# Character-Management-Tool" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Gambor27/Character-Management-Tool.git
+git push -u origin main
